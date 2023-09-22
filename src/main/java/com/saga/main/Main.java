872f52f31fb17f1ecd3f21cec6d1a223748dbac7
Walisson <walisson.marcelo2000@gmail.com>
2023-09-22 16:57:01 -0300

    Navigate to Whatsapp webpage.

--- src/main/java/com/saga/main/Main.java
+++ src/main/java/com/saga/main/Main.java
@@ -1,20 +1,29 @@
 package com.saga.main;
 
 import com.saga.excel.Excel;
+import com.saga.selenium.Selenium;
+import com.saga.whatsapp.Whatsapp;
 
 
 public class Main {
 
 	public static void main(String[] args) {
 
-		Excel excelFile = new Excel("C:\\Users\\walis\\OneDrive\\Saga\\Arquivos\\GitHub\\whatsappbot\\test.xlsx");
-		excelFile.openWorkbook();
-		excelFile.getSheetAt(0);
-		excelFile.setCellValue(1, 1, "This is a test.");
-		excelFile.saveWorkbook();
-		excelFile.closeWorkbook();
+		Selenium driver = new Selenium();
+		Whatsapp whatsapp = new Whatsapp(driver);
+		whatsapp.getPage();
 		
-		System.out.println("Done");
+		driver.wait("div.landing-title._2K09Y");
+		System.out.println("Scan the QR Code, please.");
+		while (true) {
+			try {
+				driver.findElement("div.landing-title._2K09Y");
+			} catch(Exception e) {
+				break;
+			}
+		}
+		driver.wait("button.opCKJ._28iyj");
+		System.out.println("Conversations have been loaded.");
 	
 	}
 
