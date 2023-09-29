c7808e4a50e1995e61a55f2040168188130889ea
Walisson <walisson.marcelo2000@gmail.com>
2023-09-29 20:14:00 -0300

    Implement Excel class methods.

--- src/main/java/com/saga/main/Main.java
+++ src/main/java/com/saga/main/Main.java
@@ -1,5 +1,6 @@
 package com.saga.main;
 
+import com.saga.excel.Excel;
 import com.saga.selenium.Selenium;
 import com.saga.whatsapp.Whatsapp;
 
@@ -8,6 +9,13 @@ public class Main {
 
 	public static void main(String[] args) {
 
+		// Parameters
+		String excelFilePath = "C:\\Users\\walis\\OneDrive\\Saga\\Arquivos\\GitHub\\whatsappbot\\src\\main\\resources\\data.xlsx"; 
+		int numberOfRowsInWorksheet = 3;
+		//
+	
+		
+		// Initialize Whatsapp.
 		Selenium driver = new Selenium();
 		
 		Whatsapp whatsapp = new Whatsapp(driver);
@@ -20,15 +28,21 @@ public class Main {
 		
 		driver.waitForElementToAppear("button.opCKJ._28iyj");
 		System.out.println("Conversations have been loaded.");
-//		Excel excelFile = new Excel("C:\\Users\\walis\\OneDrive\\Saga\\Arquivos\\GitHub\\whatsappbot\\test.xlsx");
-//		excelFile.openWorkbook();
-//		excelFile.getSheetAt(0);
-//		excelFile.setCellValue(1, 1, "This is a test.");
-//		excelFile.saveWorkbook();
-//		excelFile.closeWorkbook();
-//		
-//		System.out.println("Done");
 		
+		
+		// Contact people according to the worksheet.
+		Excel excelFile = new Excel(excelFilePath);
+		for (int i = 1; i <= numberOfRowsInWorksheet; i++) {
+			excelFile.openWorkbook();
+			excelFile.getSheetAt(0);
+			
+			// Code will be implemented.
+			
+			excelFile.saveWorkbook();
+			excelFile.closeWorkbook();			
+		}
+		
+		System.out.println("Done.");
 
 	}
 
